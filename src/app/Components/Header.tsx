119bbcfb96bab7ae32750a3dafe5156d9dde882e
BakrimNadia <nadia_lina@hotmail.com>
2025-01-30 21:27:52 +0100

    initial commit

---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>WELCOME TO SU SUSHI</h1>
+
+    </div>
+  );
+};
+
+export default Header;
