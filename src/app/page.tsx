565d18856f975438d88ea5ca24f21a4d3b9877f3
BakrimNadia <nadia_lina@hotmail.com>
2025-01-31 15:31:13 +0100

    Create navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 import Header from "./Components/Header";
 import Presentation from "./Components/Presentation";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <div >
       <div>
+        <Navbar />
         <img className="text-center mx-auto mt-4" src="Logo-sushi.png" alt="logo" />
       <h1 className="comfortaa text-center mt-2 mb-10 font-bold text-4xl">WELCOME TO SU SUSHI</h1>
  <Header />
