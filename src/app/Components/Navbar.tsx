88fa8f1ff5634b955dc70c328abaa121eea0d0d4
BakrimNadia <nadia_lina@hotmail.com>
2025-01-31 21:04:38 +0100

    adjust style to burger menu

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -30,29 +30,29 @@ export default function Navbar() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Product
+          <a href="#" className="text-sm/6 font-semibold text-white">
+            NOS MENUS
             </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Features
+          <a href="#" className="text-sm/6 font-semibold text-white">
+            A PROPOS
           </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Marketplace
+          <a href="#" className="text-sm/6 font-semibold text-white">
+            ADRESSE
           </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Company
+          <a href="#" className="text-sm/6 font-semibold text-white">
+            CONTACT
           </a>
         </PopoverGroup>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
-                alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                alt="logo"
+                src="Logo-sushi.png"
                 className="h-8 w-auto"
               />
             </a>
@@ -67,30 +67,30 @@ export default function Navbar() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+              <div className="space-y-2 py-6 bg-black">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50"
                 >
-                    Product
+                    NOS MENUS
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50"
                 >
-                  Features
+                  A PROPOS
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50"
                 >
-                  Marketplace
+                  ADRESSE
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50"
                 >
-                  Company
+                  CONTACT
                 </a>
               </div>
             </div>
