6f64376b5957ef547d6b226ee859b10c8f20562b
BakrimNadia <nadia_lina@hotmail.com>
2025-01-31 15:13:00 +0100

    add logo to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
   return (
     <div >
       <div>
-      <h1 className="comfortaa text-center mt-10 mb-10 font-bold text-4xl">WELCOME TO SU SUSHI</h1>
+        <img className="text-center mx-auto mt-4" src="Logo-sushi.png" alt="logo" />
+      <h1 className="comfortaa text-center mt-2 mb-10 font-bold text-4xl">WELCOME TO SU SUSHI</h1>
  <Header />
       </div>
       <div>
